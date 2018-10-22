bab6d37cd74bf9a601be95fb37636dc41eeb1f31
oosancheza <oosancheza@concredito.com.mx>
2018-10-22 15:02:54 -0600

    [MOD] se ajustaron funciones

--- src/com/mcc/Main.java
+++ src/com/mcc/Main.java
@@ -9,47 +9,89 @@ public class Main {
 
     Recursion recursion = new Recursion();
 
+    int[] a1 = {1, 2, 3, 4, 5};
+    int[] b = new int[0];
     List<Integer> numeros = new ArrayList<>();
     numeros.add(1);
     numeros.add(2);
     numeros.add(3);
     numeros.add(4);
 
+    List<Integer> numerosVacio = new ArrayList<>();
+    List<Integer> numerosNulo = null;
+
+    String[] valor1 = {"1", "2", "3", "4"};
+    String[] valor2 = {};
+
+    System.out.println("Suma de numeros de un arreglo a1:" + recursion.sumarNumerosArray(a1));
+    // System.out.println("Suma de numeros de un arreglo b:" + recursion.sumarNumerosArray(b));
+
     System.out.println("Suma de numeros de una lista:" + recursion.sumarNumerosLista(numeros));
-    System.out.println(
-        "Suma de numeros de un arreglo:" + recursion.sumarNumerosArray(new int[] {1, 2, 3, 4, 5}));
-    System.out.println(
-        "imprimir datos:" + recursion.imprimir(new String[] {"o", "s", "c", "a", "r"}));
-    System.out.println("Factorial de 5:" + recursion.factorial(5));
+    // System.out.println("Suma de numeros de una lista vacio:" +
+    // recursion.sumarNumerosLista(numerosVacio));
+    // System.out.println("Suma de numeros de una lista nulo:" +
+    // recursion.sumarNumerosLista(numerosNulo));
+
+    System.out.println("impresion a1:" + recursion.imprimir(valor1));
+    // System.out.println("impresion b:" + recursion.imprimir(valor2));
+
+    System.out.println("factorial 5!:" + recursion.factorial(5));
+    System.out.println("factorial 0!:" + recursion.factorial(0));
+    // System.out.println("factorial -1!:" + recursion.factorial(-1));
+
+    List<Integer> valoresFibonacci = recursion.listaValoresFibonacci(100);
+
+    System.out.println("Fibonacci recursivo:" + recursion.fibonacciRecursivo(10));
+    System.out.println("Fibonacci iterativo:" + recursion.fibonacciIterativo(10));
+    System.out.println("Fibonacci guardando valores:" + recursion.fibonacciGuardandoValores(10));
+
     Long empieza = System.nanoTime();
-    System.out.println("Fibonacci 5 recursivo:" + recursion.fibonacciRecursivo(5));
+    for (Integer item : valoresFibonacci) {
+      recursion.fibonacciRecursivo(item);
+    }
     Long termina = System.nanoTime() - empieza;
-    System.out.println("Tiempo que dura en milisegundos:" + (termina / 1e6));
+    System.out.println("Tiempo que dura recursivo en milisegundos:" + (termina / 1e6));
 
     empieza = System.nanoTime();
-    System.out.println("Fibonacci 6 iterativo:" + recursion.fibonacciIterativo(6));
+    for (Integer item : valoresFibonacci) {
+      recursion.fibonacciIterativo(item);
+    }
     termina = System.nanoTime() - empieza;
-    System.out.println("Tiempo que dura en milisegundos:" + (termina / 1e6));
+    System.out.println("Tiempo que dura iterativo en milisegundos:" + (termina / 1e6));
 
-    System.out.println("decimal a binario 2:" + recursion.decimalAbinario(11));
-    System.out.println("potencia 4^2:" + recursion.potencia(4, 2));
-    System.out.println("Ackerman:" + recursion.ackerman(2, 3));
-    System.out.println("numero catalan:" + recursion.numeroCatalan(5));
+    empieza = System.nanoTime();
+    for (Integer item : valoresFibonacci) {
+      recursion.fibonacciGuardandoValores(item);
+    }
+    termina = System.nanoTime() - empieza;
+    System.out.println("Tiempo que dura guardando valores en milisegundos:" + (termina / 1e6));
 
-    int[] datos = new int[] {1, 2, 3, 4, 5};
-    System.out.println(
-        "busqueda binaria:" + recursion.busquedaBinaria(datos, 0, datos.length - 1, 1));
+    int[] datos = {1, 2, 3, 5, 8};
+    System.out.println("busqueda binaria de 3:" + recursion.busquedaBinaria(datos, 3));
+    System.out.println("busqueda binaria de 10:" + recursion.busquedaBinaria(datos, 10));
+    System.out.println("busqueda binaria de 3:" + recursion.busquedaBinaria(datos, -2));
+
+    System.out.println("decimal a binario 2:" + recursion.decimalAbinario(0));
+    // System.out.println("decimal a binario 2:" + recursion.decimalAbinario(-1));
+
+    System.out.println("potencia 2^3:" + recursion.potencia(2, 3));
+    System.out.println("potencia -2^3:" + recursion.potencia(-2, 3));
+    System.out.println("potencia 2^-3:" + recursion.potencia(2, -3));
+
+    System.out.println("Ackerman 0 Y 0:" + recursion.ackerman(0, 0));
+    // System.out.println("Ackerman 4 Y 5:" + recursion.ackerman(4, 5));
+    System.out.println("numero catalan 20:" + recursion.numeroCatalan(6));
 
     System.out.println(
-        "Maximo Comun Divisor Euclides de 90 y 135: " + recursion.MCDEuclides(90, 135));
+        "Maximo Comun Divisor Euclides de 18 y 27: " + recursion.MCDEuclides(18, 27));
     System.out.println(
-        "Maximo Comun Divisor divisores de 90 y 135: " + recursion.MCDDivisores(90, 135));
+        "Maximo Comun Divisor divisores de 27 y 18: " + recursion.MCDDivisores(18, 27));
 
     System.out.println(
-        "Maximo Comun Divisor descomposicion de factores primos de 90 y 135: "
-            + recursion.MCDDescomposicionFactoresPrimos(90, 135));
+        "Maximo Comun Divisor descomposicion de factores primos de 27 y 18: "
+            + recursion.MCDDescomposicion(27, 18));
 
     System.out.println("Gray code de 4:");
-    recursion.codigoGray("", 4);
+    recursion.codigoGray(4);
   }
 }
--- src/com/mcc/Recursion.java
+++ src/com/mcc/Recursion.java
@@ -1,9 +1,6 @@
 package com.mcc;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Recursion {
 
@@ -14,7 +11,9 @@ public class Recursion {
   }
 
   public int sumarNumerosArray(int[] numeros) {
-    if (numeros.length == 1) {
+    if (numeros == null || numeros.length == 0) {
+      throw new NullPointerException("el arreglo no debe ser nulo o vacio.");
+    } else if (numeros.length == 1) {
       return numeros[0];
     }
 
@@ -22,7 +21,9 @@ public class Recursion {
   }
 
   public Integer sumarNumerosLista(List<Integer> numeros) {
-    if (numeros.size() == 1) {
+    if (numeros == null || numeros.size() == 0) {
+      throw new NullPointerException("el arreglo no debe ser nulo o vacio.");
+    } else if (numeros.size() == 1) {
       return numeros.get(0);
     }
 
@@ -30,7 +31,9 @@ public class Recursion {
   }
 
   public String imprimir(String[] datos) {
-    if (datos.length == 1) {
+    if (datos == null || datos.length == 0) {
+      throw new NullPointerException("el arreglo no debe ser nulo o vacio.");
+    } else if (datos.length == 1) {
       return datos[0];
     }
 
@@ -38,7 +41,9 @@ public class Recursion {
   }
 
   public int factorial(int numero) {
-    if (numero <= 0) {
+    if (numero < 0) {
+      throw new RuntimeException("El numero del factorial debe ser positivo.");
+    } else if (numero == 0) {
       return 1;
     }
 
@@ -46,7 +51,9 @@ public class Recursion {
   }
 
   public int fibonacciRecursivo(int n) {
-    if (n <= 0) {
+    if (n < 0) {
+      throw new RuntimeException("El numero para fibonacci debe ser positivo.");
+    } else if (n == 0) {
       return 0;
     } else if (n == 1 || n == 2) {
       return 1;
@@ -55,10 +62,18 @@ public class Recursion {
   }
 
   public int fibonacciIterativo(int n) {
+    if (n < 0) {
+      throw new RuntimeException("El numero para fibonacci debe ser positivo.");
+    } else if (n == 0) {
+      return 0;
+    } else if (n == 1 || n == 2) {
+      return 1;
+    }
+
     int nUno = 0;
     int nDos = 1;
 
-    for (int k = 1; k < n; k++) {
+    for (int i = 1; i < n; i++) {
       int aux;
       aux = nUno + nDos;
       nUno = nDos;
@@ -68,23 +83,70 @@ public class Recursion {
     return nDos;
   }
 
-  String decimalAbinario(int decimal) {
-    int residuo;
-    if (decimal == 0) {
-      return "";
+  private Map<Integer, Integer> valoresFibonacci = new HashMap<>();
+
+  public int fibonacciGuardandoValores(int n) {
+    if (n < 0) {
+      throw new RuntimeException("El numero para fibonacci debe ser positivo.");
+    } else if (n == 0) {
+      return 0;
+    } else if (n == 1 || n == 2) {
+      return 1;
     }
+
+    if (valoresFibonacci.containsKey(n)) {
+      return valoresFibonacci.get(n);
+    }
+
+    int valorUno = fibonacciGuardandoValores(n - 1);
+    valoresFibonacci.putIfAbsent(n - 1, valorUno);
+
+    int valorDos = fibonacciGuardandoValores(n - 2);
+    valoresFibonacci.putIfAbsent(n - 2, valorDos);
+
+    return valoresFibonacci.get(n - 1) + valoresFibonacci.get(n - 2);
+  }
+
+  public int busquedaBinaria(int[] datos, int valor) {
+    return busquedaBinaria(datos, 0, datos.length - 1, valor);
+  }
+
+  private int busquedaBinaria(int[] datos, int menor, int mayor, int valor) {
+    int media = (mayor + menor) / 2;
+    int valorMedio = datos[media];
+
+    if (menor > mayor) return -1;
+    else if (valorMedio == valor) return media;
+    else if (valorMedio < valor) return busquedaBinaria(datos, media + 1, mayor, valor);
+    else return busquedaBinaria(datos, menor, media - 1, valor);
+  }
+
+  public String decimalAbinario(int decimal) {
+    if (decimal < 0) {
+      throw new RuntimeException("El numero decimal a convertir debe ser positivo");
+    } else if (decimal == 0) {
+      return "0";
+    }
+
+    int residuo;
     residuo = decimal % 2;
     return (decimalAbinario(decimal / 2) + "" + residuo);
   }
 
-  public int potencia(int numero, int potencia) {
-    if (potencia == 0) {
+  public double potencia(int numero, int exponente) {
+    if (exponente == 0) {
       return 1;
     }
-    return numero * potencia(numero, potencia - 1);
+    if (exponente < 0) {
+      return 1 / potencia(numero, -exponente);
+    }
+    return numero * potencia(numero, exponente - 1);
   }
 
-  long ackerman(long valorUno, long valorDos) {
+  public long ackerman(long valorUno, long valorDos) {
+    if (valorUno < 0 || valorDos < 0) {
+      throw new RuntimeException("los valores en la funcion de ackerman no deben ser negativos");
+    }
     if (valorUno == 0) {
       return (valorDos + 1);
     } else if (valorUno > 0 && valorDos == 0) {
@@ -101,27 +163,34 @@ public class Recursion {
     return resultado + numeroCatalan(numero - 1) * (2 * ((2 * numero) - 1)) / (numero + 1);
   }
 
-  public int busquedaBinaria(int[] datos, int menor, int mayor, int valor) {
-    int media = (mayor + menor) / 2;
-    int valorMedio = datos[media];
-
-    if (menor > mayor) return -1;
-    else if (valorMedio == valor) return media;
-    else if (valorMedio < valor) return busquedaBinaria(datos, media + 1, mayor, valor);
-    else return busquedaBinaria(datos, menor, media - 1, valor);
-  }
-
   public int MCDEuclides(int valorUno, int valorDos) {
-    if (valorDos == 0) {
+
+    if (valorUno < 0 || valorDos < 0) {
+      throw new RuntimeException("Los valores para MCD deben ser positivos");
+    } else if (valorDos == 0) {
       return valorUno;
     }
     return MCDEuclides(valorDos, valorUno % valorDos);
   }
 
-  public int MCDDescomposicionFactoresPrimos(int valorUno, int valorDos) {
+  public int MCDDescomposicion(int valorUno, int valorDos) {
+    int valor;
+
+    if (valorUno > valorDos) {
+      valor = MCDDescomposicionFactoresPrimos(valorUno, valorDos);
+    } else {
+      valor = MCDDescomposicionFactoresPrimos(valorDos, valorUno);
+    }
+
+    return valor;
+  }
+
+  private int MCDDescomposicionFactoresPrimos(int valorUno, int valorDos) {
     int mcd = -1;
 
-    if (valorUno == valorDos) {
+    if (valorUno < 0 || valorDos < 0) {
+      throw new RuntimeException("Los valores para MCD deben ser positivos");
+    } else if (valorUno == valorDos) {
       mcd = valorUno;
     } else {
 
@@ -129,6 +198,7 @@ public class Recursion {
       List<Integer> factoresPrimosDos = factoresPrimos(valorDos);
 
       List<Integer> interseccion;
+
       if (factoresPrimosUno.size() < factoresPrimosDos.size()) {
         interseccion = conjunto.interseccionRepetidos(factoresPrimosUno, factoresPrimosDos);
       } else {
@@ -141,6 +211,30 @@ public class Recursion {
     }
 
     return mcd;
+  }
+
+  public int MCDDivisores(int valorUno, int valorDos) {
+    Integer valor = -1;
+
+    if (valorUno < 0 || valorDos < 0) {
+      throw new RuntimeException("Los valores para MCD deben ser positivos");
+    } else if (valorUno == valorDos) {
+      valor = valorUno;
+    } else {
+      List<Integer> divisoresUno = divisores(valorUno);
+      List<Integer> divisoresDos = divisores(valorDos);
+
+      Set<Integer> interseccion = conjunto.interseccion(divisoresUno, divisoresDos);
+
+      if (interseccion != null) {
+        for (Integer item : interseccion) {
+          if (item > valor) {
+            valor = item;
+          }
+        }
+      }
+    }
+    return valor;
   }
 
   public int multiplicarNumerosArray(List<Integer> numeros) {
@@ -163,28 +257,6 @@ public class Recursion {
     return factors;
   }
 
-  public int MCDDivisores(int valorUno, int valorDos) {
-    Integer valor = -1;
-
-    if (valorUno == valorDos) {
-      valor = valorUno;
-    } else {
-      List<Integer> divisoresUno = divisores(valorUno);
-      List<Integer> divisoresDos = divisores(valorDos);
-
-      Set<Integer> interseccion = conjunto.interseccion(divisoresUno, divisoresDos);
-
-      if (interseccion != null) {
-        for (Integer item : interseccion) {
-          if (item > valor) {
-            valor = item;
-          }
-        }
-      }
-    }
-    return valor;
-  }
-
   public List<Integer> divisores(int numero) {
     List<Integer> numDivisores = new ArrayList<>();
     for (int i = 1; i < (numero + 1); i++) {
@@ -195,7 +267,11 @@ public class Recursion {
     return numDivisores;
   }
 
-  public void codigoGray(String prefix, int n) {
+  public void codigoGray(int n) {
+    codigoGray("", n);
+  }
+
+  private void codigoGray(String prefix, int n) {
     if (n == 0) System.out.println(prefix);
     else {
       codigoGray(prefix + "0", n - 1);
@@ -209,5 +285,14 @@ public class Recursion {
       codigoGray(prefix + "1", n - 1);
       gray(prefix + "0", n - 1);
     }
+  }
+
+  public List<Integer> listaValoresFibonacci(int longitud) {
+    List<Integer> valores = new ArrayList<>();
+    for (int i = 0; i < longitud; i++) {
+      valores.add((int) (Math.random() * (25 + 1)));
+    }
+
+    return valores;
   }
 }
